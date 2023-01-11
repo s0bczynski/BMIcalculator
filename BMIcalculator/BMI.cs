62dd9c3a601a7c82ab9f6fa86c08374867afea80
sobe <sobe@DESKTOP-HU3I4VI>
2023-01-11 11:34:01 +0100

    Dodaj pliki projektów.

---
+++ BMIcalculator/BMI.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMIcalculator
+{
+	public class BMI
+	{
+		public static string getWeight()
+		{
+			Console.Clear();
+			Console.Write("Enter your weight in kg: ");
+			return Console.ReadLine();
+		}
+		public static string getHeight()
+		{
+			Console.Clear();
+			Console.Write("Enter your height in cm: ");
+			return Console.ReadLine();
+		}
+		public static void checkCondition(double bmiResult)
+		{
+			if(bmiResult < 18.5)
+			{
+				 Console.WriteLine("You're underweight - for your own health you have to gain some weight!");
+			}
+			if(bmiResult > 18.5 && bmiResult < 24.9)
+			{
+				Console.WriteLine("You're in perfect weight!");
+			}
+			if(bmiResult > 24.9 && bmiResult < 30)
+			{
+				Console.WriteLine("You're overweight - consider hitting a gym");
+			}
+			if(bmiResult > 30)
+			{
+				Console.WriteLine("You're obese! For your own health you have to lose some weight! ");
+			}
+			
+		}
+
+		
+	}
+}
---
+++ BMIcalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using BMIcalculator;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Program
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string h = BMI.getHeight();
+			string w = BMI.getWeight();
+			double h1 = double.Parse(h);
+			double w1 = double.Parse(w);
+			Console.Clear();
+
+			double bmiResult = w1 / ((h1/100) * (h1/100));
+			Console.WriteLine($"Your BMI is: {bmiResult}. ");
+			BMI.checkCondition(bmiResult);
+		}
+	}
+}
